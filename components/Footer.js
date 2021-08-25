b934167fafe9463c977c5371efa25a9a7e5d437f
Harshit <harshitaditya27@gmail.com>
2021-08-25 13:40:08 +0530

    Added Files

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5>ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+                <p>HotelTonight</p>
+                <p>Airbnb for work</p>
+                <p>Made possible by Hosts</p>
+                <p>Careers</p>
+                <p>Founder's Letter</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5>COMMUNITY</h5>
+                <p>Diversity & Belonging</p>
+                <p>Accessibility</p>
+                <p>Airbnb Associates</p>
+                <p>Frontline Stays</p>
+                <p>Guest Referrals</p>
+                <p>Airbnb.org</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5>HOST</h5>
+                <p>Host your home</p>
+                <p>Host an Online Experience</p>
+                <p>Host an Experience</p>
+                <p>Responsible hosting</p>
+                <p>Resource Centre</p>
+                <p>Community Centre</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5>SUPPORT</h5>
+                <p>Our COVID-19 Response</p>
+                <p>Cancellation options</p>
+                <p>Neighbourhood Support</p>
+                <p>Trust & Safety</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { GlobeAltIcon, MenuIcon, UserCircleIcon, UserIcon, SearchIcon } from '@heroicons/react/solid'
+function Header() {
+    return (
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+            {/*Left */}
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+                <Image src="https://links.papareact.com/qd3"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
+                />
+            </div>
+            {/*Middle - Search */}
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start yourr search" />
+                <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white p-2 rounded-full cursor-pointer md:mx-2" />
+            </div>
+            {/*Right*/}
+            <div className="flex items-center space-x-4 justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a Host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer" />
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full" >
+                    <MenuIcon className="h-6" />
+                    <UserCircleIcon className="h-6" />
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
